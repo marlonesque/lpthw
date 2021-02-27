a3664bddf4f0f0c68226b524774032c43cb7675d
Marlon Ausby <lacboi2k7@gmail.com>
2021-02-27 06:48:28 -0500

    ex1

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("Thi is fun.")
+print('Yay! Printing')
+print("I'd much rather you no'not'.")
+print('I "said" do not touch this.')
